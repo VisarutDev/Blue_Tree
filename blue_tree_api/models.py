035a9955414c3152954c8250f6ebdcd194f70ce5
Visarut Muneepetcharut <muneepetcharut.v@hotmail.com>
2023-06-02 15:01:46 +0700

    first

--- blue_tree_api/models.py
+++ blue_tree_api/models.py
@@ -1,34 +1,73 @@
 from django.db import models
 
-class UserRole(models.Model):
+class UserBooking(models.Model):
     class Meta:
-        db_table = 'user_role'
-
-    role_id = models.AutoField(primary_key=True)
-    role_name = models.CharField(max_length=55, blank=True)
+        db_table = 'user_booking'
+    booking_id = models.AutoField(primary_key=True)
+    booking_agent_com = models.CharField(max_length=55, blank=True, null=True)
+    booking_customer_first_name = models.CharField(max_length=55, blank=True, null=True)
+    booking_customer_last_name = models.CharField(max_length=55, blank=True, null=True)
+    booking_email = models.EmailField(max_length=55, blank=True, null=True)
+    booking_age = models.IntegerField(default=0)
+    booking_gender = models.CharField(max_length=10, blank=True, null=True)
+    booking_tel = models.IntegerField(default=0)
+    booking_voucher_code = models.CharField(max_length=55, blank=True, null=True, default=None)
+    booking_booking_id = models.CharField(max_length=55, blank=True, null=True, default=None)
     create_at = models.DateTimeField(auto_now_add=True, blank=True)
     update_at = models.DateTimeField(auto_now=True, blank=True)
 
     def __str__(self):
-        return str(self.role_id)
+        return str(self.info_id)
 
-class Users(models.Model):
+    def jsonFormat(self):
+        data = {
+            'booking_id' : self.booking_id,
+            'booking_agent_com' : self.booking_agent_com,
+            'booking_customer_first_name' : self.booking_customer_first_name,
+            'booking_customer_last_name' : self.booking_customer_last_name,
+            'booking_email' : self.booking_email,
+            'booking_voucher_code' : self.booking_voucher_code,
+            'booking_booking_id' : self.booking_booking_id,
+            'create_at' : self.create_at,
+            'update_at' : self.update_at
+        }
+        return data
+class TypeGroup(models.Model):
     class Meta:
-        db_table = 'users'
-    users_id = models.AutoField(primary_key=True)
-    users_role = models.ForeignKey(UserRole, related_name='users_users_role', on_delete=models.DO_NOTHING)
-    users_verify = models.BooleanField(default=False)
-    users_country_tel = models.CharField(max_length=55, blank=True, null=True)
-    users_address = models.CharField(max_length=255, blank=True, null=True)
-    users_tel = models.CharField(max_length=9, unique=True)
-    users_email = models.EmailField(max_length=55, blank=True, null=True)
-    users_password = models.CharField(max_length=255, blank=True)
-    users_facebook_id = models.CharField(max_length=55, unique=True, blank=True, null=True)
-    users_first_name = models.CharField(max_length=55, blank=True)
-    users_last_name = models.CharField(max_length=55, blank=True)
-    # users_image = models.ImageField(upload_to='img/users/', default='img/none/no_img.png', null=True, blank=True)
-    users_pdpa_status = models.IntegerField(default=0, blank=True, null=True)
-    users_onDelete = models.DateTimeField(null=True)
-    users_original_tel = models.CharField(max_length=255, blank=True, null=True)
+        db_table = 'type_group'
+    type_group_id = models.AutoField(primary_key=True)
+    type_group_people = models.IntegerField(default=0)
+    type_group_detail = models.CharField(max_length=20)
+    type_group_stauts = models.BooleanField(default=True)
+    create_at = models.DateTimeField(auto_now_add=True, blank=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True)
+class InformationDetail(models.Model):
+    class Meta:
+        db_table = 'information_detail'
+    info_detail_id = models.AutoField(primary_key=True)
+    info_detail_country = models.CharField(max_length=100, blank=True, null=True)
+    info_detail_live_phuket = models.BooleanField(default=False)
+    info_detail_people = models.IntegerField(default=0)
+    info_detail_type = models.ForeignKey(TypeGroup, related_name='info_detail_type_id', on_delete=models.DO_NOTHING, null=True, blank=True)
+    # info_detail_first_name = models.CharField(max_length=255, blank=True, null=True)
+    # info_detail_last_name = models.CharField(max_length=255, blank=True, null=True)
+    # info_detail_age = models.IntegerField(default=0)
+    # info_detail_gender = models.CharField(max_length=10, blank=True, null=True)
+    # info_detail_email = models.EmailField(max_length=55, blank=True, null=True)
+    # info_detail_tel = models.IntegerField(default=0)
+    info_detail_info = models.ForeignKey(UserBooking, related_name='info_detail_booking_id', on_delete=models.DO_NOTHING, null=True, blank=True)
+    info_detail_status = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True, blank=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True)
+class InformationDetailList(models.Model):
+    class Meta:
+        db_table = 'information_detail_list'
+    info_list_id = models.AutoField(primary_key=True)
+    info_list_first_name = models.CharField(max_length=255, blank=True, null=True)
+    info_list_last_name = models.CharField(max_length=255, blank=True, null=True)
+    info_list_age = models.IntegerField(default=0)
+    info_list_gender = models.CharField(max_length=10, blank=True, null=True)
+    info_list_info = models.ForeignKey(InformationDetail,related_name='info_list_info_id', on_delete=models.DO_NOTHING, null=True, blank=True)
+    info_list_status = models.BooleanField(default=False)
     create_at = models.DateTimeField(auto_now_add=True, blank=True)
     update_at = models.DateTimeField(auto_now=True, blank=True)
--- blue_tree_api/views.py
+++ blue_tree_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
 from .models import *
 from blue_tree.settings import db_blue_tree
 from .utils import create_access_token, create_refresh_token, verify_access_token, verify_refresh_token, Hash
@@ -58,4 +60,154 @@ class Apicheck(APIView):
             data = request.data
             return Response(data)
         else:
+            return Response(False)
+
+class GetBookingByAgent(APIView):
+    def get(self, request):
+        try:
+            data = request.GET.get
+            agent = data("agent_name") if data("agent_name") is not None else None
+            voucher_code = data("voucher_code") if data("voucher_code") is not None else None
+            booking_id = data("booking_id") if data("booking_id") is not None else None
+            if agent != None:
+                filter = Q(booking_agent_com = agent)
+            elif voucher_code != None:
+                filter = Q(booking_voucher_code = voucher_code)
+            elif booking_id != None:
+                filter = Q(booking_booking_id = booking_id)
+            booking_data = UserBooking.objects.using(db_blue_tree).get(filter).jsonFormat()
+            res = {
+                'msg' : True,
+                'data' : booking_data
+            }
+            return Response(res,status = status.HTTP_200_OK)
+        except:
+            return Response(False,status = status.HTTP_401_UNAUTHORIZED)
+
+class GetBookingByChoose(APIView):
+    def get(self, request):
+        try:
+            data = request.GET.get
+            choose = data('type_choose')
+            if choose == 1: #agent
+                agent_code = data('agent_code')
+                voucher_code = data('voucher_code')
+                filter = Q(booking_agent_com = agent_code) & Q(booking_voucher_code = voucher_code)
+            elif choose == 2: #gropup
+                print("test222")
+            elif choose == 3: #OTAs
+                booking_number = data('booking_number')
+                filter = Q(booking_booking_id = booking_number)
+            elif choose == 4: #line
+                booking_number = data('booking_number')
+                filter = Q(booking_booking_id = booking_number)
+            elif choose == 5: #walk in
+                voucher_code = data('voucher_code')
+                filter = Q(booking_voucher_code = voucher_code)
+            elif choose == 6: #member
+                first_name = data('first_name')
+                last_name = data('last_name')
+                filter = Q(booking_customer_first_name = first_name) and Q(booking_customer_last_name = last_name)
+            elif choose == 7: #voucher
+                voucher_code = data('voucher_code')
+                filter = Q(booking_voucher_code = voucher_code)
+            get_booking = UserBooking.objects.using(db_blue_tree).filter(filter).jsonFormat()
+            res = {
+                'msg' : True,
+                'data' : get_booking
+            }
+            return Response(res,status=status.HTTP_200_OK)
+        except:
+            return Response(False,status=status.HTTP_401_UNAUTHORIZED)
+
+class FromForDetails(APIView):
+    def get(self, request): # get draft
+        try:
+            data = request.GET.get
+            booking = data("booking_id")
+            booking_id = UserBooking.objects.using(db_blue_tree).get(booking_booking_id = booking).jsonFormat()
+            info = InformationDetailList.objects.using(db_blue_tree).filter(info_detail_info = booking_id.booking_id).values()
+            res = {
+                'msg' : True,
+                'data' : {
+                    'booking' : booking_id,
+                    'information' : info
+                }
+            }
+            return Response(res,status=status.HTTP_200_OK)
+        except:
+            return Response({'msg':False},status=status.HTTP_401_UNAUTHORIZED)
+
+    def post(self, request): # save draft and create detail booking
+        try:
+            data_guest_list = []
+            data = request.data
+            guests = data['guest']
+            data_detail = {
+                'info_detail_country' : data['country'] if "country" in data else None,
+                'info_detail_live_phuket' : data['live_in'] if "live_in" in data else None,
+                'info_detail_people' : data['people'] if "people" in data else None,
+                'info_detail_type' : data['with'] if "with" in data else None
+            }
+            data_booking = {
+                'booking_first_name' : guests[0]['first_name'],
+                'booking_last_name' : guests[0]['last_name'],
+                'booking_age' : guests[0]['age'],
+                'booking_gender' : guests[0]['gender'],
+                'booking_email' : guests[0]['Email'],
+                'booking_tel' : guests[0]['tel']
+            }
+            # UserBooking.objects.using(db_blue_tree).filter(booking_booking_id = data['booking_id']).update_or_create(**data_booking)
+            # InformationDetail.objects.using(db_blue_tree).create(**data_detail)
+            del guests[0]
+            if data['people'] < 10:
+                for guest in guests:
+                    data_list = {
+                            'info_list_first_name' : guest['first_name'],
+                            'info_list_last_name' : guest['last_name'],
+                            'info_list_age' : guest['age'],
+                            'info_list_gender' : guest['gender'],
+                            'info_list_info' : data['booking_id']
+                        }
+                    # InformationDetailList.objects.using(db_blue_tree).create(**data_list)
+                    data_guest_list.append(data_list)
+            else:
+                pass
+
+            res = {
+                'msg' : True,
+                'data' : {
+                    'detail' : data_detail,
+                    'gest_list' : data_guest_list,
+                    'booking' : data_booking
+                }
+            }
+            return Response(res,status=status.HTTP_200_OK)
+        except:
+            return Response(False,status=status.HTTP_401_UNAUTHORIZED)
+
+class UpdateStatusPolicy(APIView):
+    def put(self, request):
+        try:
+            data = request.data
+            booking_id = data['booking_id']
+            booking = InformationDetail.objects.using(db_blue_tree).filter(info_detail_info_id = booking_id).update(info_detail_status = True).values()
+            user_list = InformationDetailList.objects.using(db_blue_tree).filter(info_list_info_id = booking_id).update(info_list_status = True).values()
+            res = {
+                'msg' : True,
+                'data' : {
+                    'booking' : booking,
+                    'user_list' : user_list
+                }
+            }
+            return Response(res,status=status.HTTP_200_OK)
+        except:
+            return Response(False,status=status.HTTP_401_UNAUTHORIZED)
+
+class GetTypeGroup(APIView):
+    def get(self):
+        try:
+            type = TypeGroup.objects.using(db_blue_tree).all().values()
+            return Response(type)
+        except:
             return Response(False)
